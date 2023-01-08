ba1e80e7abddef72ae3420b7b7f88f10e6b62298
Elvis Carrasco <elvissamir18@gmail.com>
2023-01-08 07:51:22 -0400

    Add handleInputChange method in useCustomerForm hook

--- src/hooks/customerForm/useCustomerForm.ts
+++ src/hooks/customerForm/useCustomerForm.ts
@@ -1,14 +1,32 @@
 import { ChangeEvent } from "react"
+import { CustomerData } from "../../core/customer/customer.interfaces"
 import useCustomerFormData from "./useCustomerFormData"
 
 const useCustomerForm = () => {
     const { customerData, setCustomerData } = useCustomerFormData()
 
     const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-        console.log('Changing input', e.target.id)
+        let ncustomerData: CustomerData = {...customerData}
+        
+        if (e.target.id === 'first_name')
+            ncustomerData.first_name = e.target.value
+        
+        if (e.target.id === 'last_name')
+            ncustomerData.last_name =e.target.value
+        
+        if (e.target.id === 'age')
+            ncustomerData.age = parseInt(e.target.value)
+        
+        if (e.target.id === 'email')
+            ncustomerData.email = e.target.value
+        
+        if (e.target.id === 'company')
+            ncustomerData.company = e.target.value
+        
+        setCustomerData(ncustomerData)
     }
 
-    const handleSendData = () => {
+    const handleSendData = async () => {
         console.log('send data')
     }
 
